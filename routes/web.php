7a554461908f21a2a21d582b63bbcdeeb0f8f908
vagrant <vagrant@homestead>
2019-09-02 17:45:33 +0000

    Routes

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,37 @@ Route::get('/', function () {
 
 Route::get('hello/{name}', function($name) {
 
-	return view('hello', compact('name'));
+	return redirect()->route("prod_single");
+	// return view('hello', compact('name'));
 });
 
 // Route::get('/users', 'UserController@index');
 // Route::get('/users/{id}', 'UserController@show');
-Route::resource('/users', 'UserController');
+// Route::resource('/users', 'UserController');
+// Route::resource('/products', 'Test\ProductsController');
+
+Route::namespace("Test")->prefix("prod")->name("prod_")->group(function() {
+
+	Route::get("/", 'ProductsController@index')->name("index");
+	Route::get("/1", 'ProductsController@show')->name("single");
+});
+
+Route::view('/view', 'hello', ['name' => 'odair']);
+
+Route::get('show/{name?}/{lastName?}', function($name = null, $lastName = null) {
+
+	return $name . " " . $lastName ;
+});
+
+// Route::prefix('products')->name('products_')->group(function() {
+
+// 	Route::get('/', function() {
+
+// 		return "Products index";
+// 	})->name("index");
+
+// 	Route::get('/1', function() {
+
+// 		return "Products 1";
+// 	})->name("single");
+// });
